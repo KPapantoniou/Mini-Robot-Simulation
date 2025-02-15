e641f724bb3e5362c337040eb1a9c311dcefbb92
Konstantino Papantoniou-Xatzigiosis <kostaspapantouan01@gmail.com>
2025-02-15 23:45:50 +0200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -39,22 +39,13 @@ void Robot::addMotor(Motor& motor) {
     motors.push_back(&motor); 
 }
 
-// void Robot::calculateMovment(double t, bool is_asychronus, double dt){
-//     // cout<<"t in f in robot alcMovme: "<<t<<"\n";
-//     if(!is_asychronus){
-//         // calculate_synchronus_movment(t,dt);
-//         calculate_asynchronous_movement(t,dt);
-//     }else{
-//         calculate_asynchronous_movement(t,dt);
-//     }
-// }
 
 pair<double,double> Robot::calculate_single_motor(double t, double dt){
     force_x = 0.0;
     force_z = 0.0;
     
     double force_motor = 0.0;
-    // cout<<"t in f in robot: "<<t<<"\n";
+
     double omega_left = motors[0]->get_omega();
     double theta_left = motors[0]->get_theta();
 
@@ -84,7 +75,7 @@ pair<double,double> Robot::calculate_single_motor(double t, double dt){
 }
 
 pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) {
-    // cout << "In calculate_asynchronus"<<endl;
+
     force_x = 0.0;
     force_z = 0.0;
     moment_x = 0.0;
@@ -97,12 +88,10 @@ pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) 
     
     double omega_left = motors[0]->get_omega();
     double theta_left = motors[0]->get_theta();
-    // cout << "First motor success"<<endl;
+   
     double omega_right = motors[1]->get_omega();
     double theta_right = motors[1]->get_theta();
-    // cout << "Geting the motors values\t" << omega_left << "\t" << omega_right << endl;
-    // auto forces_left = forceCalculator.calculate_robot_forces(omega_left, omega_left*t, t);
-    // auto forces_right = forceCalculator.calculate_robot_forces(omega_right, omega_right*t, t);
+ 
     auto forces_left = forceCalculator.calculate_robot_forces(omega_left, theta_left, t);
     auto forces_right = forceCalculator.calculate_robot_forces(omega_right, theta_right, t);
 
@@ -115,7 +104,7 @@ pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) 
     force_z = force_z_left + force_z_right;
 
     auto moments = forceCalculator.calculate_torques({omega_left, omega_right}, {theta_left, theta_right}, l / 2);
-    // auto moments = forceCalculator.calculate_torques({omega_left, omega_right}, {omega_left*t, omega_right*t}, l / 2);
+    
 
     moment_x = moments.first;  
     moment_z = moments.second; 
@@ -124,13 +113,7 @@ pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) 
     double normal_force_A = total_weight / 3.0 + (moment_x *  cos(angle_A)) / (3.0 * (l/2));
     double normal_force_B = total_weight / 3.0 + (moment_x *  cos(angle_B)) / (3.0 * (l/2));
     double normal_force_C = total_weight / 3.0 + (moment_x *  cos(angle_C)) / (3.0 * (l/2));
-    // double normal_force_A = total_weight / 3.0;
-    // double normal_force_B = total_weight / 3.0;
-    // double normal_force_C = total_weight / 3.0;
-
-    // double normal_force_A = total_weight / 3.0 ;
-    // double normal_force_B = total_weight / 3.0 ;
-    // double normal_force_C = total_weight / 3.0 ;
+    
 
     double sum_normal_forces = normal_force_A + normal_force_B + normal_force_C;
     
@@ -148,10 +131,7 @@ pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) 
     double friction_A = friction_calculator(vel_A, force_x*cos(angle_A), normal_force_A);  
     double friction_B = friction_calculator(vel_B, force_x*cos(angle_B), normal_force_B);
     double friction_C = friction_calculator(vel_C, force_x*cos(angle_B), normal_force_C);
-    // cout<<"Friction A: "<<friction_A<<"\n"
-    // <<"vel_A_x: "<<vel_A_x<<"\n"
-    // <<"vel_A_y: "<<vel_A_y<<"\n";
-
+   
     force_x += -(friction_A * cos(angle_A) + friction_B * cos(angle_B) + friction_C*cos(angle_C)); 
 
     force_y = -(friction_A * sin(angle_A) + friction_B * sin(angle_B) + friction_C * sin(angle_C));
@@ -170,11 +150,11 @@ pair<double,double> Robot::calculate_asynchronous_movement(double t, double dt) 
     theta_displacement += omega * dt + 0.5 * acc_rot * dt * dt;
 
     acc_x = linear_acceleration_calculator(force_x, mass);
-    // acc_rot = linear_acceleration_calculator(force_z, mass);
+    
     acc_y = linear_acceleration_calculator(force_y, mass);
 
     vel_x = velocity_calculator(acc_x, vel_x, dt);
-    // omega = velocity_calculator(acc_rot, omega, dt);
+    
     vel_y = velocity_calculator(acc_y, vel_y, dt);
 
     pos_x = position_calculator(vel_x, pos_x, acc_x, dt);
